0c747093b228e8adc36beca0cccb07ae0ff2d223
jimmy93 <jimmy93@users.noreply.github.com>
2021-06-05 22:54:40 +0800

    Update covid.py
    This code to extract last row from csv https://raw.githubusercontent.com/wnarifin/covid-19-malaysia/master/covid-19_my.csv
    output to json format.

--- covid.py
+++ covid.py
@@ -1,35 +1,11 @@
-import requests
-import sys
 import csv
+import requests
 import json
-
-url = 'https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv'
+url = 'https://raw.githubusercontent.com/wnarifin/covid-19-malaysia/master/covid-19_my.csv' #read csv file
 resp = requests.get(url)
 decoded_content = resp.content.decode('utf-8')
-reader = csv.DictReader(decoded_content.splitlines(), delimiter=',')
-
-states = {'Pennsylvania', 'New Jersey'}
-state = sys.argv[1]
-county = {}
+reader = csv.DictReader(decoded_content.splitlines(), delimiter = ',')
+list_from_csv = []  #make a list
 for row in reader:
-    if row['state'] == state:
-        county[row['county']+'_cases'] = int(row['cases'])
-        county[row['county']+'_deaths'] = int(row['deaths'])    
-        county['last_date'] = row['date']
-        
-# CSV is in sorted order, so whatever is left is the most recent.
-
-# Calulcate state-level info
-state_cases = 0
-state_deaths = 0
-for key, val in county.items():
-    if key.endswith('deaths'):
-        state_deaths += val
-    elif key.endswith('cases'):
-        state_cases += val
-
-county['state_level_cases'] = state_cases
-county['state_level_deaths'] = state_deaths
-
-# This will print out the dict in json format for easy parsing in HA.
-print(json.dumps(county))
+    list_from_csv.append(row)
+print(json.dumps(list_from_csv[-1]))  #print json format for the last row -1
